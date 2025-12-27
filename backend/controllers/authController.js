2b736f2033ceff81d3e3fad5c4285c615782ce28
Sannia Jean <53579933+Sanniaj@users.noreply.github.com>
2025-12-27 09:26:40 -0800

    fixed syntax error

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,7 +5,7 @@ import User from '../models/User.js';
 
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES || "7d:,
+        expiresIn: process.env.JWT_EXPIRES || "7d:",
     });
 };
 
